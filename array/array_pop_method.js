d9688a93f6bdd8b09dfd48fe2679ab8400ffc3e8
Shahin Ahmed <shahinahmed7165@gmail.com>
2024-08-13 18:57:01 +0600

    update

--- array/array_pop_method.js
+++ array/array_pop_method.js
@@ -42,9 +42,4 @@ console.log(nums.length);
 nums.pop();
 console.log(nums.length);// Output: 3
 
-// let nums = [10, 20, 30, 40, 50];
-// console.log(nums.length);
-// nums.pop();
-// console.log(nums.length);
-// nums.pop();
-// console.log(nums.length);// Output: 3
+
--- array/array_push_method.js
+++ array/array_push_method.js
@@ -1,6 +1,26 @@
-let nums = [10, 20, 30, 40, 50];
-console.log(nums.length);
-nums.pop();
-console.log(nums.length);
-nums.pop();
-console.log(nums.length);// Output: 3
+// Create an array called fruits containing the strings 'apple', 'banana', and 'orange'. Use the push method to add 'grape' and 'pear' to the end of this array.
+
+let fruits = ['apple', 'banana', 'orange'];
+fruits.push('grape', 'pear');
+console.log(fruits); // Output: ['apple', 'banana', 'orange', 'grape', 'pear']
+
+
+// You have an empty array called numbers. Use the push method to add the numbers 1, 2, 3, 4, and 5 to this array in one go.
+let numbers = [];
+numbers.push(1, 2, 3, 4, 5);
+console.log(numbers); // Output: [1, 2, 3, 4, 5]
+
+
+// Given an array of numbers, use push to add the number 100 to the array only if the array length is less than 5.
+let nums = [1, 2, 3];
+if (nums.length < 5) {
+  nums.push(100);
+}
+console.log(numbers); // Output: [1, 2, 3, 100]
+
+
+//Create an array called students. Use push to add an object with properties name and age representing a student to this array. The object should have the name 'John' and the age 20.
+let students = [];
+students.push({ name: 'John', age: 20 });
+console.log(students); // Output: [{ name: 'John', age: 20 }]
+
